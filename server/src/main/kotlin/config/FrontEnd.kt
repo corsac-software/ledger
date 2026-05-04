0b527d1c110e4f86a239e245af9c3ff3ae3c3b2e
Bruno Ribeiro <brunoribeiro.sch@gmail.com>
2026-05-03 23:02:58 -0300

    refactor: Separação codigo server e web

--- server/src/main/kotlin/config/FrontEnd.kt
+++ server/src/main/kotlin/config/FrontEnd.kt
@@ -3,15 +3,13 @@ package br.dev.brunorsch.config
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
-import io.ktor.server.routing.openapi.OperationHiddenAttributeKey
-import io.ktor.server.routing.openapi.hide
-import io.ktor.utils.io.ExperimentalKtorApi
+import io.ktor.utils.io.*
 
 @OptIn(ExperimentalKtorApi::class)
 fun Application.configureReact() {
     routing {
         singlePageApplication {
-            react("web-app/dist")
+            react("../web/dist")
         }
     }
 }
