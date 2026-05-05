c05432b23adf5d7e39bb51b440e3a5181c0ec5bb
Bruno Ribeiro <brunoribeiro.sch@gmail.com>
2026-05-05 00:42:23 -0300

    fix(server): Path de migrations

--- server/src/main/kotlin/config/Flyway.kt
+++ server/src/main/kotlin/config/Flyway.kt
@@ -4,7 +4,7 @@ import org.flywaydb.core.Flyway
 import javax.sql.DataSource
 
 const val GENERATED_MIGRATIONS_DIR = "generated-migrations"
-const val MIGRATIONS_DIR = "migrations"
+const val MIGRATIONS_DIR = "server/migrations"
 
 fun setupFlyway(
     dataSource: DataSource
